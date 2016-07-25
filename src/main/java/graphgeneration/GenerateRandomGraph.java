67fa72b3d9674bcdefbcfea42a4d0344bfad32ba
Adam Torzs <adam434@gmail.com>
2016-07-25 19:17:11 +0200

    Remade graph generation to create a grid instead of a random graph.

--- src/main/java/graphgeneration/GenerateRandomGraph.java
+++ src/main/java/graphgeneration/GenerateRandomGraph.java
@@ -14,11 +14,13 @@ import java.util.Set;
  * @author Adam Torzs Generates a random planar, simple, undirected graph and
  *         saves it to a .dot file
  * 
- *         Currently it generates random points, and creates random edges between them *
+ *         Currently it generates random points, and creates random edges
+ *         between them *
  */
 public class GenerateRandomGraph {
 
 	private final int NODE_NUM = 10;
+	private final int GRID_SIZE = 4;
 	private final double EDGE_DENSITY = 0.3;
 
 	private final int MAP_SIZE = 3000;
@@ -44,10 +46,20 @@ public class GenerateRandomGraph {
 	 * Generates the required number of nodes
 	 * 
 	 */
+	/*
+	 * public List<Node> generateNodes() { ArrayList<Node> nodes = new
+	 * ArrayList<Node>(); for (int i = 0; i < NODE_NUM; i++) {
+	 * nodes.add(randomNode(i)); } return nodes; }
+	 */
 	public List<Node> generateNodes() {
-		ArrayList<Node> nodes = new ArrayList<Node>();
-		for (int i = 0; i < NODE_NUM; i++) {
-			nodes.add(randomNode(i));
+		List<Node> nodes = new ArrayList<>();
+
+		for (int i = 0; i < GRID_SIZE; i++) {
+			for (int j = 0; j < GRID_SIZE; j++) {
+				double x = (double)i / GRID_SIZE * MAP_SIZE;
+				double y = (double)j / GRID_SIZE * MAP_SIZE;
+				nodes.add(new Node(x, y, i * GRID_SIZE + j));
+			}
 		}
 		return nodes;
 	}
@@ -57,72 +69,57 @@ public class GenerateRandomGraph {
 	 * Generates the edges for the given nodes
 	 * 
 	 */
-	private List<Edge> generateEdges(List<Node> nodes)
-	{
-		List<Edge> edges = new ArrayList<Edge>();
-		int nodeNum = nodes.size();
-		for(int i = 0;i<nodeNum;i++)
-		{
-			for(int j = i;j<nodeNum;j++)
-			{
-				if(i==j) continue;
-				
-				if(random.nextDouble()<EDGE_DENSITY)
-				{
-					edges.add(new Edge(nodes.get(i),nodes.get(j)));
-					edges.add(new Edge(nodes.get(j),nodes.get(i)));
-				}
-			}
-		}
-		return edges;
-	}
-	
-	
 	/*
+	 * private List<Edge> generateEdges(List<Node> nodes) { List<Edge> edges =
+	 * new ArrayList<Edge>(); int nodeNum = nodes.size(); for(int i =
+	 * 0;i<nodeNum;i++) { for(int j = i;j<nodeNum;j++) { if(i==j) continue;
+	 * 
+	 * if(random.nextDouble()<EDGE_DENSITY) { edges.add(new
+	 * Edge(nodes.get(i),nodes.get(j))); edges.add(new
+	 * Edge(nodes.get(j),nodes.get(i))); } } } return edges; }
+	 */
+
 	private List<Edge> generateEdges(List<Node> nodes) {
+		List<Edge> edges = new ArrayList<>();
+		for (int i = 0; i < GRID_SIZE; i++) {
+			for (int j = 0; j < GRID_SIZE; j++) {
+				Node n = nodes.get(i * GRID_SIZE + j);
+				if (i != GRID_SIZE - 1) {
+					Node n2 = nodes.get((i + 1) * GRID_SIZE + j);
+					edges.add(new Edge(n, n2));
+					edges.add(new Edge(n2, n));
 
-		List<Edge> edges = new ArrayList<Edge>();
-		int n = nodes.size();
-
-		for (int i = 0; i < n; i++) {
-			for (int j = i + 1; j < n; j++) {
-				for (int k = j + 1; k < n; k++) {
-					boolean isTriangle = true;
-					for (int a = 0; a < n; a++) {
-						if (a == i || a == j || a == k)
-							continue;
-						if (nodes.get(a).insideTriangle(nodes.get(i),
-								nodes.get(j), nodes.get(k))) {
-							isTriangle = false;
-							break;
-						}
-					}
-					if (isTriangle) {
-						Edge e, f, g;
-						e = new Edge(nodes.get(i), nodes.get(j));
-						f = new Edge(nodes.get(i), nodes.get(k));
-						g = new Edge(nodes.get(j), nodes.get(k));
-						if (!edges.contains(e)) {
-							edges.add(e);
-						}
-						if (!edges.contains(f)) {
-							edges.add(f);
-						}
-						if (!edges.contains(g)) {
-							edges.add(g);
-						}
-					}
+				}
+				if (j != GRID_SIZE - 1) {
+					Node n2 = nodes.get(i * GRID_SIZE + j + 1);
+					edges.add(new Edge(n, n2));
+					edges.add(new Edge(n2, n));
 				}
 			}
 		}
 		return edges;
 	}
-	*/
+
+	/*
+	 * private List<Edge> generateEdges(List<Node> nodes) {
+	 * 
+	 * List<Edge> edges = new ArrayList<Edge>(); int n = nodes.size();
+	 * 
+	 * for (int i = 0; i < n; i++) { for (int j = i + 1; j < n; j++) { for (int
+	 * k = j + 1; k < n; k++) { boolean isTriangle = true; for (int a = 0; a <
+	 * n; a++) { if (a == i || a == j || a == k) continue; if
+	 * (nodes.get(a).insideTriangle(nodes.get(i), nodes.get(j), nodes.get(k))) {
+	 * isTriangle = false; break; } } if (isTriangle) { Edge e, f, g; e = new
+	 * Edge(nodes.get(i), nodes.get(j)); f = new Edge(nodes.get(i),
+	 * nodes.get(k)); g = new Edge(nodes.get(j), nodes.get(k)); if
+	 * (!edges.contains(e)) { edges.add(e); } if (!edges.contains(f)) {
+	 * edges.add(f); } if (!edges.contains(g)) { edges.add(g); } } } } } return
+	 * edges; }
+	 */
 
 	public void generate(String filename) throws FileNotFoundException {
 		List<Node> nodes = generateNodes();
 		List<Edge> edges = generateEdges(nodes);
-		
 
 		String start = "digraph mapgraph {";
 		String nodeTemplate = "n%d[p=\"%.1f,%.1f\"]";
@@ -137,18 +134,16 @@ public class GenerateRandomGraph {
 		pw.println(start);
 		pwapos.println(start);
 		for (Node n : nodes) {
-			String node = String.format(Locale.ENGLISH,nodeTemplate, n.num, n.x, n.y);
+			String node = String.format(Locale.ENGLISH, nodeTemplate, n.num, n.x, n.y);
 			pw.println(node);
-			String nodeApos = String.format(Locale.ENGLISH,nodeAposTemplate, n.num, n.x, n.y);
+			String nodeApos = String.format(Locale.ENGLISH, nodeAposTemplate, n.num, n.x, n.y);
 			pwapos.println(nodeApos);
 		}
 
 		for (Edge e : edges) {
-			String edge = String.format(Locale.ENGLISH,edgeTemplate, e.e1.num, e.e2.num,
-					300.0f);
+			String edge = String.format(Locale.ENGLISH, edgeTemplate, e.e1.num, e.e2.num, 300.0f);
 			pw.println(edge);
-			String edgeApos = String.format(Locale.ENGLISH,edgeAposTemplate, e.e1.num, e.e2.num,
-					300.0f);
+			String edgeApos = String.format(Locale.ENGLISH, edgeAposTemplate, e.e1.num, e.e2.num, 300.0f);
 			pwapos.println(edgeApos);
 		}
 		pw.println(end);
